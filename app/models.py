16b2e0bfd2512ce89c9361ca72f006912061fc53
IProxymate <iiidonaldiii@gmail.com>
2020-09-26 21:44:23 +0300

    first commit

--- app/models.py
+++ app/models.py
@@ -6,4 +6,11 @@ User = get_user_model()
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts") 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+
+
+
+
+
+
